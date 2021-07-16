91e6ba1132377ec17d59a313782800a6d4549016
basuabhirup <basu.abhirup@yahoo.com>
2021-07-16 23:58:23 +0530

    Modified Total per head logic and some input functionalities

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ $(".tip-input").click(function() { // adds a handler function against the "click
 
 // When any number is typed in the custom Tip-% field
 $(".tip-input.custom").keyup(function() { // adds a handler function against "keyup" event
-  tipPercantage = parseInt(this.value, 10) / 100; // saves the typed custom Tip-% value
+  tipPercantage = parseFloat(this.value, 10) / 100; // saves the typed custom Tip-% value
   calculateTip(tipPercantage); // calls the custom function
 })
 
 // When any number is typed in the Number-of-people field
 $("#no-of-people").keyup(function() { // adds a handler function against "keyup" event
+  calculateTip(tipPercantage); // calls the custom function
+})
+
+$("#no-of-people").click(function() { // adds a handler function against "keyup" event
+  calculateTip(tipPercantage); // calls the custom function
+})
+
+$("#bill-amount").keyup(function() { // adds a handler function against "keyup" event
+  calculateTip(tipPercantage); // calls the custom function
+})
+
+$("#bill-amount").click(function() { // adds a handler function against "keyup" event
   calculateTip(tipPercantage); // calls the custom function
 })
 
@@ -35,13 +47,15 @@ function calculateTip(tipPercantage) {
   var billAmount = parseFloat($("#bill-amount").val(), 10);
   var tipTotal = billAmount * tipPercantage;
   var numPeople = parseFloat($("#no-of-people").val(), 10); // holds the value of number of people
+  var billPerHead = billAmount / numPeople;
   var tipPerHead = tipTotal / numPeople;
+  var totalPerHead = billPerHead + tipPerHead;
 
   if (numPeople > 0 && Number.isInteger(numPeople)) { // checks whether no-of-people is an integer
 
     if (tipPercantage > 0) {
       $("#tip-per-head").text(parseFloat(tipPerHead.toFixed(2)));
-      $("#tip-total").text(parseFloat(tipTotal.toFixed(2)));
+      $("#tip-total").text(parseFloat(totalPerHead.toFixed(2)));
       $(".reset-button").removeClass("disabled");
     }
 
